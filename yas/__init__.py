a4ba73dae18e7b1d4daf4d533294805451a7bc14
Schlueter <bs@schlueter.blue>
2017-03-26 02:54:41 -0400

    Add regexhandler

--- yas/__init__.py
+++ yas/__init__.py
@@ -15,3 +15,14 @@ class YasError(Exception):
 class HandlerError(YasError):
     def __init__(self, msg):
         logger.log.warn(msg)
+
+class RegexHandler(YasHandler):
+
+    def __init__(self, regexp_string, bot_name, api_call, log=print):
+        super().__init__(bot_name, api_call, log=log)
+        self.regexp = re.compile(regexp_string)
+        self.log('INFO', f"{self.__class__} initialized and matching {regexp_string}!")
+
+    def test(self, data):
+        self.current_match = self.regexp.search(data.get('text'))
+        return self.current_match
