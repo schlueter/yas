e7b1cdb416e1653c7e161ddcc52efebcd1329c2c
Cara Warner <warnercara@gmail.com>
2017-07-31 15:13:46 -0400

    Make slack commands case insensitive.

--- yas/__init__.py
+++ yas/__init__.py
@@ -14,7 +14,7 @@ class RegexHandler(YasHandler):
 
     def __init__(self, regexp_string, bot):
         super().__init__(bot)
-        self.regexp = re.compile(regexp_string)
+        self.regexp = re.compile(regexp_string, flags=re.IGNORECASE)
         self.bot.log.info(f"{self.__class__} initialized and matching {regexp_string}!")
         bot_id = self.bot.retrieve_user_id(self.bot.config.bot_name)
         self.at_bot = "<@" + bot_id + ">"
