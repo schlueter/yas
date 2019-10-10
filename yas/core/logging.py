31489c630231dfbe98ab8fb4e74b842c9105ced6
Schlueter <b@schlueter.blue>
2019-10-09 18:25:59 -0700

    Hastily remove systemd use; add Dockerfile

--- yas/core/logging.py
+++ yas/core/logging.py
@@ -1,11 +1,7 @@
 import logging
 
-from systemd import journal
-
-
 class Logger:
 
     def __init__(self, log_level='WARNING'):
         self.log = logging.getLogger('yas')
-        self.log.addHandler(journal.JournaldLogHandler())
         self.log.setLevel(logging._nameToLevel[log_level])
