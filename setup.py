2baec3c27de0311fdedad60c971adc154c8857a6
Schlueter <b@schlueter.blue>
2019-10-09 23:02:18 -0700

    Simplify setup.py by removing dependency on pbr

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 # Copyright 2017 Brandon Schlueter
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    setup_requires=['pbr>=1.8'],
-    pbr=True)
+    name='yas',
+    summary='Yet Another Slack bot',
+    version='2.0-rc0',
+    packages=find_packages(),
+    install_requires=[
+        'PyYAML>=3.12',
+        'slackclient==1.0.5',
+        'websocket-client==0.40.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'yas = yas.core.application:run'
+        ]
+    },
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python",
+    ]
+)
