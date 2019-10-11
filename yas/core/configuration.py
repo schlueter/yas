83b36320094d82495d0855b897b13491d869762b
Schlueter <b@schlueter.blue>
2019-10-11 10:33:24 -0700

    Include buildin handlers by default

--- yas/core/configuration.py
+++ yas/core/configuration.py
@@ -4,7 +4,14 @@ PARAMETERS = dict(
     ignored_types=['desktop_notification', 'user_typing'],
     slack_app_token=None,
     bot_name=None,
-    handler_list=['yas.default_handler'],
+    handler_list=[
+        'yas.handlers.ignored_types_handler.',
+        'yas.handlers.not_talking_to_bot_handler.',
+        'yas.handlers.help_handler.',
+        'yas.handlers.identify_handler.',
+        'yas.handlers.rules_handler.',
+        'yas.handlers.default_handler.'
+    ],
     debug=False,
     default_response='Sure...write some more code then I can do that!',
     log_level='WARN',
